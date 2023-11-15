9eb9bf150391435c1448cefb6c98d735b40b84b1
Khotso-Bore <u19180642@tuks.co.za>
2023-11-15 20:36:02 +0200

    removed bus bounds logic as we no longer have bounds

--- frontend/src/components/BusMap.tsx
+++ frontend/src/components/BusMap.tsx
@@ -1,57 +1,46 @@
-import { MapContainer, Marker, Polyline, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Polyline,
+  TileLayer,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect, useState } from "react";
 import { getBusesOnRoute } from "../services/BusService";
 import { BusPosition } from "../Types/BusPositions";
 import busStopIcon2 from "../assets/bus-stop-2.png";
-import { Bound, Stop, Path as BusPath } from "../Types/BusRoute";
-
-const isBusOnRoute = (
-  routeBounds: Bound[],
-  busPosition: BusPosition
-): boolean => {
-  const polyPoints = routeBounds;
-
-  // const x = -25.96776600812863;
-  // const y = 28.122686107264043;
-  const x = busPosition.latitude;
-  const y = busPosition.longitude;
-
-  let inside = false;
-  for (let i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
-    let xi = polyPoints[i].lat;
-    let yi = polyPoints[i].lng;
-
-    let xj = polyPoints[j].lat;
-    let yj = polyPoints[j].lng;
-
-    let intersect =
-      yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-    if (intersect) inside = !inside;
-  }
-  //console.log(inside)
-
-  return inside;
-};
+import busIcon from "../assets/busIcon4.png";
+import { Stop } from "../Types/BusRoute";
 
 interface Props {
   stops: Stop[];
-  bounds: Bound[];
-  path: BusPath[];
+  stationCoordinates: number[];
+  path: number[][];
 }
 
-export const BusMap = ({ path, bounds, stops }: Props) => {
+interface ChangeViewProps {
+  center: number[];
+  zoom: number;
+}
+
+export const BusMap = ({ path, stationCoordinates, stops }: Props) => {
   const [busses, SetBusses] = useState<BusPosition[]>();
+  const [mapCenter, SetMapCenter] = useState([0, 0]);
 
   useEffect(() => {
+    //console.log("hello mapsa");
+    //minimap.setView([-25.746644695069936, 28.23931115432058]);
     const getBusses = async () => {
       const response = await getBusesOnRoute();
       console.log(response);
       if (response) {
-        const busPositions = response.Result.busPositions.filter((x) =>
-          isBusOnRoute(bounds, x)
-        );
+        const busPositions = response.Result.busPositions;
+
+        // .filter((x) => {
+        //   if (bounds) isBusOnRoute(bounds, x);
+        // });
         SetBusses(busPositions);
       }
     };
@@ -63,20 +52,37 @@ export const BusMap = ({ path, bounds, stops }: Props) => {
     return () => clearInterval(interval);
   }, []);
 
+  const ChangeView = ({ center, zoom }: ChangeViewProps) => {
+    const map = useMap();
+
+    const oldCenter = mapCenter[0];
+    //console.log(oldCenter, center[0]);
+    if (oldCenter != center[0]) {
+      SetMapCenter([center[0], center[1]]);
+      map.setView([center[0], center[1]], zoom);
+    }
+    return null;
+  };
+
   return (
     <MapContainer
-      center={[-25.97156600812863, 28.121686107264043]}
+      className="z-0"
+      center={[stationCoordinates[0], stationCoordinates[1]]}
       zoom={14}
       scrollWheelZoom={true}
     >
+      <ChangeView
+        center={[stationCoordinates[0], stationCoordinates[1]]}
+        zoom={14}
+      />
       <Polyline
         pathOptions={{ color: "DodgerBlue", weight: 8 }}
-        positions={path.map((x) => [x.lat, x.lng])}
+        positions={path.map((x) => [x[1], x[0]])}
       />
       {stops.map((stop) => (
         <Marker
-          key={stop.id}
-          position={[stop.lat, stop.lng]}
+          key={stop.code}
+          position={[stop.latLng[0], stop.latLng[1]]}
           icon={
             new L.Icon({
               iconUrl: busStopIcon2,
@@ -88,7 +94,16 @@ export const BusMap = ({ path, bounds, stops }: Props) => {
       {busses === undefined
         ? null
         : busses!.map((bus) => (
-            <Marker key={bus.busId} position={[bus.latitude, bus.longitude]} />
+            <Marker
+              key={bus.busId}
+              position={[bus.latitude, bus.longitude]}
+              icon={
+                new L.Icon({
+                  iconUrl: busIcon,
+                  iconSize: new L.Point(25, 25),
+                })
+              }
+            />
           ))}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
