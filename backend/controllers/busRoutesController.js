5f14530fd4136114ddc091c18f6da21efebfe4a0
Khotso-Bore <u19180642@tuks.co.za>
2023-11-15 20:38:56 +0200

    added new endpoint for fetching specific route

--- backend/controllers/busRoutesController.js
+++ backend/controllers/busRoutesController.js
@@ -2,7 +2,20 @@ const BusRoute = require("../models/BusRoute");
 
 const getRoute = async (req, res) => {
   try {
-    const response = await BusRoute.findOne();
+    const city = req.query.city;
+    const area = req.query.area;
+    const response = await BusRoute.findOne({ city, area });
+
+    //console.log(response);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getRoutes = async (req, res) => {
+  try {
+    const response = await BusRoute.find({});
 
     //console.log(response);
     res.status(200).json(response);
@@ -13,4 +26,5 @@ const getRoute = async (req, res) => {
 
 module.exports = {
   getRoute,
+  getRoutes,
 };
